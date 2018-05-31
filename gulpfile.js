9c1d8e8230d672250b6ad0520e42128f747437bd
Songle <951540966@qq.com>
2018-05-31 17:31:06 +0800

    add formatter config file and change gulp-ruby-sass to gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
-const gulp = require('gulp');
-const sass = require('gulp-ruby-sass');
-const shelljs = require('shelljs');
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const shelljs = require('shelljs')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 // 静态服务器
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', function() {
 	browserSync.init({
 		server: {
-			baseDir: "./generate-output/html"
+			baseDir: './generate-output/html'
 		}
-	});
-});
+	})
+})
 function compileSass() {
-	sass('src/genetation/html-template/0/src/scss/*.scss')
-		.on('error', sass.logError)
+	gulp
+		.src('src/genetation/html-template/0/src/scss/*.scss')
+		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('generate-output/html/css'))
 }
-gulp.task('compile', function () {
+gulp.task('compile', function() {
 	shelljs.exec('cnpm start')
 	compileSass()
-	var watcher = gulp.watch('src/genetation/**');
-	watcher.on('change', function (event) {
+	var watcher = gulp.watch('src/genetation/**')
+	watcher.on('change', function(event) {
 		console.log('ReCompiled Temaplate....')
 		shelljs.exec('cnpm start')
 		compileSass()
 		reload()
-	});
-});
+	})
+})
 // gulp.task('sass', ['compile'], () =>
 // 	sass('src/genetation/html-template/0/src/scss/*.scss')
 // 		.on('error', sass.logError)
 // 		.pipe(gulp.dest('generate-output/html/css'))
 // );
 
-gulp.task('default', ['compile', 'browser-sync']);
+gulp.task('default', ['compile', 'browser-sync'])
