24eb89bcc6528c5837435ac02382cccb3121055a
Songle <951540966@qq.com>
2018-07-09 17:02:03 +0800

    add genetate project shell

--- create_project.js
+++ create_project.js
@@ -3,12 +3,18 @@ const config = require('./config')
 //# 删除controller
 execSh('rm -rf javaweb-project/project/System/src/com/system/controller')
 execSh('mkdir -p javaweb-project/project/System/src/com/system/controller')
-execSh('echo "test"')
 execSh(
 	`cp .${
 		config.outputDir
 	}/ssm_controller/* javaweb-project/project/System/src/com/system/controller`
 )
+execSh('rm -rf javaweb-project/project/System/WebRoot/components')
+execSh('rm -rf javaweb-project/project/System/WebRoot/css')
+execSh('rm -rf javaweb-project/project/System/WebRoot/img')
+execSh('rm -rf javaweb-project/project/System/WebRoot/js')
+execSh('rm -rf javaweb-project/project/System/WebRoot/json')
+execSh('rm -rf javaweb-project/project/System/WebRoot/pages')
 execSh(
 	`cp -Rf .${config.outputDir}/html/* javaweb-project/project/System/WebRoot`
 )
+console.log('Generate Project Done.')
